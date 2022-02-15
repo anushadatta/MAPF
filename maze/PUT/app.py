d0656a0f7d805811c251b718a2b093382f1d99f0
anushadatta <anushadatta@gmail.com>
2022-02-15 14:55:53 +0800

    API DB integration

--- maze/PUT/app.py
+++ maze/PUT/app.py
@@ -2,41 +2,39 @@ import json
 
 # import requests
 
+from decimal import Decimal
+import boto3
+import os
+
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
 
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
+    dynamodb = boto3.resource('dynamodb')
 
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+    TABLE_NAME = os.environ.get("TABLE")
+    table = dynamodb.Table(TABLE_NAME)
 
-    context: object, required
-        Lambda Context runtime methods and attributes
+    event_body = json.loads(event['body'])
+    maze_id = event_body['maze_id']
+    maze_string = event_body['maze_string']
 
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
+    response = table.update_item(
+        Key={
+            'maze_id': maze_id,
+        },
+        UpdateExpression="SET maze_string=:x",
+        ExpressionAttributeValues={
+            ':x': maze_string,
+        },
+    )
 
     return {
         "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
-        }),
+        'headers': {
+            'Access-Control-Allow-Headers': 'Content-Type,Authorization,x-api-key',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+        },
+        "body": json.dumps(response),
     }
+
