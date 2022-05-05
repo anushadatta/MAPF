8369b256ed61e6857b0536e64163a3626c87ffb0
anushadatta <anushadatta@gmail.com>
2022-05-05 22:14:26 +0800

    confirmatory dialog box css

--- js/colors.js
+++ js/colors.js
@@ -1,18 +1,18 @@
 // set up color array
 const agent_colors = [
-    ['#990000', '#CC0000'], // red
-    ['#006600', '#009900'], // dark green
-    ['#009999', '#00CCCC'], // turquoise
-    ['#0066CC', '#0080FF'], // blue
-    ['#B266FF', '#CC99FF'], // lilac
-    ['#33FFFF', '#99FFFF'], // aqua blue
-    ['#00CC00', '#33FF33'], // light green
-    ['#0000CC', '#0000FF'], // ultramarine blue
-    ['#990099', '#CC00CC'], // dark pink
-    ['#FFFF00', '#FFFF66'], // yellow
-    ['#CC0066', '#FF007F'], // magenta
-    ['#CC6600', '#FF8000'], // orange 
-    ['#FF66FF', '#FF99FF'],  // pink
-    ['#999900', '#CCCC00'], // olive
-    ['#6600CC', '#7F00FF'], // purple
+    ['#990000', '#CC0000'], // 1 red
+    ['#006600', '#009900'], // 2 dark green
+    ['#009999', '#00CCCC'], // 3 turquoise
+    ['#0066CC', '#0080FF'], // 4 blue
+    ['#B266FF', '#CC99FF'], // 5 lilac
+    ['#33FFFF', '#99FFFF'], // 6 aqua blue
+    ['#00CC00', '#33FF33'], // 7 light green
+    ['#0000CC', '#0000FF'], // 8 ultramarine blue
+    ['#990099', '#CC00CC'], // 9 dark pink
+    ['#FFFF00', '#FFFF66'], // 10 yellow
+    ['#CC0066', '#FF007F'], // 11 magenta
+    ['#CC6600', '#FF8000'], // 12 orange 
+    ['#FF66FF', '#FF99FF'],  // 13 pink
+    ['#999900', '#CCCC00'], // 14 olive
+    ['#6600CC', '#7F00FF'], // 15 purple
 ]
